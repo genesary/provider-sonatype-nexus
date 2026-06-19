6c2705678b4a2da97ba30df517ce91f26bc22c2e
BoxBoxJason <contact@boxboxjason.dev>
2026-06-19 17:48:38 +0200

    test: add unit and e2e tests
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>

---
+++ internal/fake/license_client.go
@@ -0,0 +1,55 @@
+// Package fake provides mock implementations for testing.
+package fake
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	iamclient "github.com/genesary/provider-sonatype-nexus/internal/clients/iam"
+)
+
+// errNotImplemented is returned when a mock function has not been configured.
+var errNotImplemented = errors.New("mock function not implemented")
+
+// MockLicenseClient is a mock iamclient.LicenseClient for unit tests.
+type MockLicenseClient struct {
+	GetLicenseFn     func(ctx context.Context) (*iamclient.LicenseInfo, error)
+	InstallLicenseFn func(ctx context.Context, licenseData []byte) error
+	DeleteLicenseFn  func(ctx context.Context) error
+}
+
+// Ensure MockLicenseClient implements LicenseClient.
+var _ iamclient.LicenseClient = &MockLicenseClient{}
+
+// NewMockLicenseClient returns a new MockLicenseClient.
+func NewMockLicenseClient() *MockLicenseClient {
+	return &MockLicenseClient{}
+}
+
+// GetLicense implements LicenseClient.
+func (m *MockLicenseClient) GetLicense(ctx context.Context) (*iamclient.LicenseInfo, error) {
+	if m.GetLicenseFn != nil {
+		return m.GetLicenseFn(ctx)
+	}
+
+	return nil, errNotImplemented
+}
+
+// InstallLicense implements LicenseClient.
+func (m *MockLicenseClient) InstallLicense(ctx context.Context, licenseData []byte) error {
+	if m.InstallLicenseFn != nil {
+		return m.InstallLicenseFn(ctx, licenseData)
+	}
+
+	return errNotImplemented
+}
+
+// DeleteLicense implements LicenseClient.
+func (m *MockLicenseClient) DeleteLicense(ctx context.Context) error {
+	if m.DeleteLicenseFn != nil {
+		return m.DeleteLicenseFn(ctx)
+	}
+
+	return errNotImplemented
+}
